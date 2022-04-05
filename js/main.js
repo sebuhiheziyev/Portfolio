52e1d86c8d47002aee7a047d903ec1288b3a7a51
Sabuxhi <sebuhiheziyev39@mail.com>
2022-04-04 17:03:46 -0700

    change design and worked post page

--- js/main.js
+++ js/main.js
@@ -1,13 +1,44 @@
 "use strict";
-$(window).scroll(function (event) {
-    var scroll = $(window).scrollTop();
-    var viewportWidth = $(window).width();
-    if(viewportWidth > 900){
-        if(scroll > 431){
-            $('.side-bar').addClass('side-bar-sticky')
+$(window).on('load', function(){
+
+    //for responsive menu 
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        var viewportWidth = $(window).width();
+        if(viewportWidth > 900){
+            if(scroll > 431){
+                $('.side-bar').addClass('side-bar-sticky')
+            }
+            else{
+                $('.side-bar').removeClass('side-bar-sticky')
+            }
         }
-        else{
-            $('.side-bar').removeClass('side-bar-sticky')
+    });
+
+    var activSliderCount = 1;
+    changeSlider(activSliderCount);
+
+    $(".beforeImage").click(function(){
+        activSliderCount -=1;
+        changeSlider(activSliderCount);
+    });
+
+    $(".afterImage").click(function(){
+        activSliderCount +=1;
+        changeSlider(activSliderCount);
+    });
+
+    function changeSlider (n){
+        var sliders = document.getElementsByClassName("my-slider");
+        if(n > sliders.length){activSliderCount = 1}
+        if(n < 1){activSliderCount = sliders.length}
+        for (var i = 0; i < sliders.length; i++) {
+            sliders[i].style.display = "none";
         }
+        sliders[activSliderCount-1].style.display = "block";
     }
+   
 });
+
+
+
